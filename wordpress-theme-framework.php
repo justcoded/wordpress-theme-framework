0fa0f5d1f41a958cd9a21232f3a3c1881a214a5c
Alex Prokopenko <ab.prokopenko@gmail.com>
2018-08-22 16:21:37 +0300

    Add version info for pagebuilder updates

--- wordpress-theme-framework.php
+++ wordpress-theme-framework.php
@@ -4,7 +4,7 @@
 Plugin Name: WordPress Theme Framework
 Description: Lightweight theme framework base with Model-View concept for developers who want to better organize their own custom themes.
 Tags: mvc theme, theme boilerplate, oop theme, mini framework
-Version: 2.2.0
+Version: 2.3.0
 Author: JustCoded / Alex Prokopenko
 Author URI: http://justcoded.com/
 License: GPL3
@@ -16,9 +16,9 @@ defined( 'JTF_PLUGIN_FILE' ) || define( 'JTF_PLUGIN_FILE', __FILE__ );
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 // Required spl autoload registration class.
-require_once( dirname( __FILE__ ) . '/framework/Autoload.php' );
+require_once dirname( __FILE__ ) . '/framework/Autoload.php';
 
-// Include small helper functions
-require_once( dirname( __FILE__ ) . '/framework/helpers.php' );
+// Include small helper functions.
+require_once dirname( __FILE__ ) . '/framework/helpers.php';
 
 new JustCoded\WP\Framework\Autoload( 'JustCoded\WP\Framework', dirname( __FILE__ ) . '/framework' );
