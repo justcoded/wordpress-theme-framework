65471bca355fdb1062a9574b9971b4a3322cf0df
Alex Prokopenko <ab.prokopenko@gmail.com>
2018-09-11 19:55:13 +0300

    Renamed ACF support class

--- framework/ACF/ACF_Support.php
+++ framework/ACF/ACF_Support.php
@@ -5,7 +5,7 @@ namespace JustCoded\WP\Framework\ACF;
 
 use JustCoded\WP\Framework\Objects\Singleton;
 
-class ACF_Supports {
+class ACF_Support {
 	use Singleton;
 	
 	// TODO: register collapse flexible.
