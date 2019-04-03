07a9d239a893e8cff7d05df05966c1050887a39e
Nikita Dudarev <ndudarev@justcoded.co>
2019-04-03 16:44:50 +0300

    update basic class

--- framework/Objects/RestController.php
+++ framework/Objects/RestController.php
@@ -3,12 +3,29 @@
 namespace JustCoded\WP\Framework\Objects;
 
 
+/**
+ * Class RestController
+ * @package JustCoded\WP\Framework\Objects
+ *
+ * @method RestController instance() static
+ */
 abstract class RestController {
 	use Singleton;
 
-	static $namespace = 'api';
+	/**
+	 * @var string
+	 */
+	protected $namespace = 'api';
 
+	/**
+	 * @var array
+	 */
 	protected $routes = [];
+
+	/**
+	 * @var array
+	 */
+	protected $slugs;
 
 	/**
 	 * Constructor
@@ -17,30 +34,74 @@ abstract class RestController {
 	 * @throws \Exception Missing $ROUTE property.
 	 */
 	protected function __construct() {
-		echo self::$namespace;
-		if ( empty( self::$namespace ) ) {
-			throw new \Exception( 'REST ENDPOINT "' . get_class( $this ) . '" init failed: missing namespace property' );
+		if ( empty( $this->namespace ) ) {
+			throw new \Exception( 'REST ENDPOINT "' . static::class . '" init failed: missing namespace property' );
 		}
 
 		add_action( 'rest_api_init', [ $this, 'register_endpoint' ] );
-
 	}
 
-	public static function get_permalink( $slug, $args = array() ) {
-		$slug = str_replace( [ '(', ')' ], [ '(\(', '\))' ], $slug );
-		echo $slug;
-		//$slug = preg_replace($slug, $args)
-		echo home_url( '/wp-json/' . self::$namespace );
+	/**
+	 * Get_permalink
+	 *
+	 * @param string $slug_key .
+	 * @param array  $args .
+	 *
+	 * @return string|bool
+	 */
+	public function get_permalink( $slug_key, $args = array() ) {
+		if ( is_array( $slug_key ) ) {
+			return false;
+		}
+
+		$data       = [];
+		$slug       = $this->slugs[ $slug_key ];
+		$slug_regex = str_replace( [ '(', ')' ], [ '_*(', ')*_' ], $slug );
+		$home_url   = home_url( '/wp-json/' . $this->namespace );
+
+		if ( ! preg_match_all( '/<(\w+)>/i', $slug, $args_matches ) ) {
+			return false;
+		}
+
+		if ( ! preg_match_all( '/_\*(.*?)\*_/i', $slug_regex, $data_matches ) ) {
+			return false;
+		}
+
+		foreach ( $args_matches[1] as $match ) {
+			if ( array_key_exists( $match, $args ) ) {
+				$data[ $match ] = $args[ $match ];
+				unset( $args[ $match ] );
+			}
+		}
+
+		$query_args = add_query_arg( [ $args ] );
+
+		foreach ( $data_matches[1] as $match ) {
+			if ( ! preg_match( '/<(\w+)>/i', $match, $key_match ) ) {
+				continue;
+			}
+
+			if ( ! empty( $data[ $key_match[1] ] ) ) {
+				$slug = str_replace( $match, $data[ $key_match[1] ], $slug );
+			}
+		}
+
+		return $home_url . $slug . $query_args;
 	}
 
+	/**
+	 * Register_endpoint
+	 *
+	 * @throws \Exception
+	 */
 	public function register_endpoint() {
 		$this->init();
 		if ( empty( $this->routes ) ) {
-			throw new \Exception( 'REST ENDPOINT "' . get_class( $this ) . '" init failed: missing routes property' );
+			throw new \Exception( 'REST ENDPOINT "' . static::class . '" init failed: missing routes property' );
 		}
 		foreach ( $this->routes as $route ) {
 			register_rest_route(
-				self::$namespace,
+				$this->namespace,
 				$route['route'],
 				$route['args']
 			);
@@ -48,24 +109,49 @@ abstract class RestController {
 	}
 
 	/**
-	 * Declaration of the 'init' action hook
-	 * should call $this->add_route( $method, $route, $callback, $args ) inside
+	 * Add_route
+	 *
+	 * @param string $route
+	 * @param string $method
+	 * @param array  $callback
+	 * @param array  $args
+	 * @param array  $permission_callback
+	 *
+	 * @throws \Exception
 	 */
-	abstract public function init();
+	protected function add_route( $route, $method, $callback, $args = [], $permission_callback = [] ) {
 
-	protected function add_route( $method = 'POST', $route, $callback, $args = [] ) {
 		if ( empty( $route ) ) {
-			return;
+			throw new \Exception( static::class . ' class: $route property is required' );
 		}
 
 		$this->routes[] = [
 			'route' => $route,
 			'args'  => [
-				'methods'  => $method,
-				'callback' => $callback,
-				'args'     => $args
-			]
+				'methods'             => $method,
+				'callback'            => $callback,
+				'permission_callback' => $permission_callback,
+				'args'                => $args
+			],
 		];
 	}
 
+	/**
+	 * Response
+	 *
+	 * @param mixed $data
+	 * @param int   $status
+	 * @param array $headers
+	 *
+	 * @return \WP_REST_Response
+	 */
+	protected function response( $data, $status = 200, $headers = [] ) {
+		return new \WP_REST_Response( $data, $status, $headers );
+	}
+
+	/**
+	 * Declaration of the 'init' action hook
+	 * should call $this->add_route( $method, $route, $callback, $args ) inside
+	 */
+	abstract public function init();
 }
